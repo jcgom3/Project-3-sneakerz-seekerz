3934f92716ccf593856ce6f17e608472beefa583
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-01-29 12:02:02 -0500

    started most of the typeDefs file, might need some updating later

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,58 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Product {
+        _id: ID
+        name: String
+        brand: String
+        sku: String
+        price: Int
+    }
+
+    type Brand {
+        _id: ID
+        name: String
+    }
+
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        orders: [Order]
+    }
+
+    type: Auth: {
+        token: ID
+        user: User
+    }
+
+    type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+    }
+
+    type Query {
+        brands: [Brand]
+        products(brand: ID, name: String): [Product]
+        product(_id: ID!): Product
+        user: User
+        order(_id: ID!): Order
+        checkout(products: [ID]!): Checkout
+    }
+
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
+    }
+
+    type: Checkout {
+        session: ID
+    }
+`;
 
 module.exports = typeDefs;
