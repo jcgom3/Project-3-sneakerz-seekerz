9ad183dcfd37b4d23c3c752a5162c686c8c66e73
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2021-02-02 18:28:18 -0500

    added footer title

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,14 +10,15 @@ function Footer() {
         position: "fixed",
         left: "0",
         bottom: "0",
-        height: "100px",
         width: "100%",
         opacity: "0.5",
-        color: "white"
+        color: "white",
+        fontWeight: "bold"
     };
 
     return(
         <footer style={style}>
+            <div>Shoe factory</div>
             <a href="/"><img src="https://img.icons8.com/bubbles/50/000000/github.png"/></a>
             <a href="/"><img src="https://img.icons8.com/bubbles/50/000000/linkedin.png"/></a>
             <a href="/"><img src="https://img.icons8.com/bubbles/50/000000/gmail.png"/></a>
