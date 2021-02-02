f9d96d94f3b9f6e38109a3c24c6b9937ec540444
Juan  Gomez <jcgom3@gmail.com>
2021-02-01 23:01:04 -0500

    added order and index.js to models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Product = require('./Product');
+const Brand = require('./Brand');
+const Order = require('./Order');
+
+module.exports = { User, Product, Brand, Order };
