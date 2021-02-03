35a9a9007541add0049a58705d193fc95d2ba55f
Juan  Gomez <jcgom3@gmail.com>
2021-02-03 01:35:23 -0500

    wrapped provier for store on the app component

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
