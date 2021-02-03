6670eb61ca8ec230d60991d3a1a167237d676e3c
Juan  Gomez <jcgom3@gmail.com>
2021-02-03 02:51:11 -0500

    added ./ to service worker relative path on line 124

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -121,7 +121,7 @@ const isLocalhost = Boolean(
   }
   
   export function unregister() {
-    if ('serviceWorker' in navigator) {
+    if ('./serviceWorker' in navigator) {
       navigator.serviceWorker.ready
         .then(registration => {
           registration.unregister();
