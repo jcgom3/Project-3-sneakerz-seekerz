7bc5a06617915b32f462adc29ef3b7d38ca3e002
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-02-04 01:49:49 -0500

    updated seeds.js images file path

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -43,7 +43,7 @@ const products = await Product.insertMany([
     {
         name: 'Jordan 4 Retro Fire Red',
         description: 'Remastered retro Jordan 4 in white with red accents.',
-        image: '/../images/shoe-4.jpg',
+        image: 'shoe-4.jpg',
         brand: brands[1]._id,
         price: 200,
         quantity: 8
@@ -51,7 +51,7 @@ const products = await Product.insertMany([
     {
         name: 'Jordan 1 Pine Green Black',
         description: 'Jordan 1 with black and pine green upper',
-        image: '/../images/shoe-5.jpg',
+        image: 'shoe-5.jpg',
         brand: brands[1]._id,
         price: 170,
         quantity: 11
@@ -59,7 +59,7 @@ const products = await Product.insertMany([
     {
         name: 'Jordan 1 Court Purple',
         description: 'Jordan 1 with white and purple upper',
-        image: '/../images/shoe-6.jpg',
+        image: 'shoe-6.jpg',
         brand: brands[1]._id,
         price: 170,
         quantity: 6
@@ -67,7 +67,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Air Max 95 Neon',
         description: 'Gray and Neon green air max 95',
-        image: '/../images/shoe-7.jpg',
+        image: 'shoe-7.jpg',
         brand: brands[3]._id,
         price: 170,
         quantity: 9
@@ -75,7 +75,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Air Max 97 Neon Seoul',
         description: 'Black Air Max 97 with a twist',
-        image: '/../images/shoe-8.jpg',
+        image: 'shoe-8.jpg',
         brand: brands[3]._id,
         price: 170,
         quantity: 3
@@ -83,7 +83,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Air Max 1 Anniversary Royal',
         description: 'The original Air Max 1 sihlouette in white and red',
-        image: '/../images/shoe-9.jpg',
+        image: 'shoe-9.jpg',
         brand: brands[3]._id,
         price: 140,
         quantity: 12
@@ -91,7 +91,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Air Max 1 Anniversary Royal',
         description: 'The original Air Max 1 sihlouette in white and royal blue',
-        image: '/../images/shoe-10.jpg',
+        image: 'shoe-10.jpg',
         brand: brands[3]._id,
         price: 140,
         quantity: 16
@@ -99,7 +99,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike React Element 87 Sail',
         description: 'Previous shoe of the year winner',
-        image: '/../images/shoe-11.jpg',
+        image: 'shoe-11.jpg',
         brand: brands[3]._id,
         price: 160,
         quantity: 4
@@ -107,7 +107,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike React Element 87 Moss',
         description: 'Green tones on this see-through shoe',
-        image: '/../images/shoe-12.jpg',
+        image: 'shoe-12.jpg',
         brand: brands[3]._id,
         price: 160,
         quantity: 5
@@ -115,7 +115,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Dunk High Maize',
         description: 'Nike Dunks in black and yellow',
-        image: '/../images/shoe-13.jpg',
+        image: 'shoe-13.jpg',
         brand: brands[3]._id,
         price: 120,
         quantity: 9
@@ -123,7 +123,7 @@ const products = await Product.insertMany([
     {
         name: 'Nike Dunk High Michigan',
         description: 'Hightop Nike Dunks in University of Michigans class maize and blue',
-        image: '/../images/shoe-14.jpg',
+        image: 'shoe-14.jpg',
         brand: brands[3]._id,
         price: 120,
         quantity: 8
@@ -155,7 +155,7 @@ const products = await Product.insertMany([
     {
         name: 'Adidas Yeezy Boost 350 v2 Earth',
         description: 'Earth and sand inspired version of the 350 v2 model',
-        image: '/../images/shoe-18.jpg',
+        image: 'shoe-18.jpg',
         brand: brands[2]._id,
         price: 220,
         quantity: 18
@@ -163,7 +163,7 @@ const products = await Product.insertMany([
     {
         name: 'Yeezy Boost 700 Inertia',
         description: 'Light blue colorway of the original Yeezy 700',
-        image: '/../images/shoe-19.jpg',
+        image: 'shoe-19.jpg',
         brand: brands[2]._id,
         price: 300,
         quantity: 7
@@ -171,7 +171,7 @@ const products = await Product.insertMany([
     {
         name: 'Yeezy 700 Mauve',
         description: 'Earth-toned Yeezy 700 sihlouette',
-        image: '/../images/shoe-20.jpg',
+        image: 'shoe-20.jpg',
         brand: brands[2]._id,
         price: 300,
         quantity: 5
@@ -179,7 +179,7 @@ const products = await Product.insertMany([
     {
         name: 'Adidas Ultra Boost 4.0 White',
         description: 'Ultra Boost running shoe with white upper',
-        image: '/../images/shoe-21.jpg',
+        image: 'shoe-21.jpg',
         brand: brands[2]._id,
         price: 180,
         quantity: 9
@@ -187,7 +187,7 @@ const products = await Product.insertMany([
     {
         name: 'Adidas Ultra Boost 1.0 Black',
         description: 'Ultra Boost running shoe with black upper',
-        image: '/../images/shoe-22.jpg',
+        image: 'shoe-22.jpg',
         brand: brands[2]._id,
         price: 200,
         quantity: 13
@@ -195,7 +195,7 @@ const products = await Product.insertMany([
     {
         name: 'Adidas Stan Smith White Green',
         description: 'Original Adidas sihlouette with green detailing',
-        image: '/../images/shoe-23.jpg',
+        image: 'shoe-23.jpg',
         brand: brands[2]._id,
         price: 60,
         quantity: 6
@@ -203,7 +203,7 @@ const products = await Product.insertMany([
     {
         name: 'Adidas Stan Smith White Navy',
         description: 'Original Adidas sihlouette with navy detailing',
-        image: '/../images/shoe-24.jpg',
+        image: 'shoe-24.jpg',
         brand: brands[2]._id,
         price: 60,
         quantity: 9
@@ -211,7 +211,7 @@ const products = await Product.insertMany([
     {
         name: 'Clyde Core Foil Mens Sneakers White',
         description: 'Classic white sneakers with black accent',
-        image: '/../images/shoe-25.jpg',
+        image: 'shoe-25.jpg',
         brand: brands[0]._id,
         price: 70,
         quantity: 12
@@ -219,7 +219,7 @@ const products = await Product.insertMany([
     {
         name: 'Clyde Core Foil Mens Sneakers Black',
         description: 'Classic black sneakers with white accent',
-        image: '/../images/shoe-26.jpg',
+        image: 'shoe-26.jpg',
         brand: brands[0]._id,
         price: 70,
         quantity: 9
@@ -227,7 +227,7 @@ const products = await Product.insertMany([
     {
         name: 'RS-Fast Metal v2 Sneakers Gold',
         description: 'Black and gold running shoes',
-        image: '/../images/shoe-27.jpg',
+        image: 'shoe-27.jpg',
         brand: brands[0]._id,
         price: 110,
         quantity: 7
@@ -235,7 +235,7 @@ const products = await Product.insertMany([
     {
         name: 'Fuse Training Shoes White',
         description: 'Lowtop white trainers',
-        image: '/../images/shoe-28.jpg',
+        image: 'shoe-28.jpg',
         brand: brands[0]._id,
         price: 90,
         quantity: 11
@@ -243,7 +243,7 @@ const products = await Product.insertMany([
     {
         name: 'Fuse Training Shoes Black',
         description: 'Lowtop black trainers',
-        image: '/../images/shoe-29.jpg',
+        image: 'shoe-29.jpg',
         brand: brands[0]._id,
         price: 90,
         quantity: 8
@@ -251,7 +251,7 @@ const products = await Product.insertMany([
     {
         name: 'Speedcat Motorsport Shoes Red',
         description: "Red athletic trainers",
-        image: '/../images/shoe-30.jpg',
+        image: 'shoe-30.jpg',
         brand: brands[0]._id,
         price: 90,
         quantity: 10
